dce7665eeb04edfb4f6652ca0e6721ca07d33ad0
kvasenko <kvasenko@ukr.net>
2016-10-25 16:20:47 +0300

    Added dump.sql

---
+++ src/dump.sql
@@ -0,0 +1,20 @@
+# Categories for all modules
+CREATE TABLE categories
+(
+  id BIGINT(20) unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  rootId BIGINT(20) unsigned,
+  parentId BIGINT(20) unsigned,
+  name VARCHAR(255) NOT NULL,
+  alias VARCHAR(255) NOT NULL,
+  created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated TIMESTAMP NOT NULL,
+  CONSTRAINT categories_categories_id_fk FOREIGN KEY (parentId) REFERENCES categories (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+CREATE UNIQUE INDEX UNIQUE_alias ON categories (parentId, alias);
+
+LOCK TABLES `acl_privileges` WRITE;
+/*!40000 ALTER TABLE `acl_privileges` DISABLE KEYS */;
+
+INSERT INTO `acl_privileges` (`roleId`, `module`, `privilege`)
+VALUES
+  (2,'categories','Management');
